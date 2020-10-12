5a89c4048200168da5be3e57dfb349e74a5c8de6
Osmund <21172332+osmundf@users.noreply.github.com>
2020-10-12 18:13:16 +0200

    Updated test for mutable value collection class.

--- test-kotlin/app/zoftwhere/mutable/value/MutableValueCollectionTest.kt
+++ test-kotlin/app/zoftwhere/mutable/value/MutableValueCollectionTest.kt
@@ -2,6 +2,7 @@ package app.zoftwhere.mutable.value
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import java.util.NoSuchElementException
@@ -25,5 +26,40 @@ internal class MutableValueCollectionTest {
         assertNotNull(actual)
         assertNotNull(actual.get())
         assertEquals(value, actual.get())
+    }
+
+    @Test
+    fun testAccept() {
+        val value = mutableListOf<CharSequence>("one", "two", "three")
+        val actual = MutableValueCollection<CharSequence>()
+        actual.set(value)
+        assertEquals(value, actual.get())
+    }
+
+    @Test
+    fun testOptional() {
+        val list = mutableListOf("one", "two", "three")
+        val actual = MutableValueCollection(list)
+        assertTrue(actual.optional().isPresent)
+    }
+
+    @Test
+    fun testEmpty() {
+        val actual = MutableValueCollection<String>()
+        assertTrue(actual.isEmpty)
+    }
+
+    @Test
+    fun testPresent() {
+        val list = mutableListOf("one")
+        val actual = MutableValueCollection(list)
+        assertEquals(list[0], actual.get(0).get())
+    }
+
+    @Test
+    fun testStream() {
+        val list = mutableListOf("one")
+        val actual = MutableValueCollection(list)
+        assertEquals(1, actual.stream().count())
     }
 }
